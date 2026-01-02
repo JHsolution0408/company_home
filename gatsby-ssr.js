6817e441be35d300b6257952f8dc29a4652eb5f6
jh-eunhee <eunhee.ju@jh-solution.net>
2026-01-02 14:49:28 +0900

    feature: Update opengraph settings (#4)
    * feature: Add opengraph image
    
    * feature: Update thumbnail
    
    * feature: Update seo component
    
    * feature: Add sitemap plugin
    
    * feature: Update opengraph content
    
    * feature: Update og title

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,27 +4,10 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
 
-const React = require('react')
-const { siteMetadata } = require('./gatsby-config')
-
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
-  setHtmlAttributes({ lang: `en` })
-
-  const baseUrl = (siteMetadata && siteMetadata.siteUrl) || ''
-  const normalizedBase = baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl
-  const ogImageUrl = `${normalizedBase}/images/og-image.png`
-
-  setHeadComponents([
-    // Open Graph
-    React.createElement('meta', { key: 'og-image', property: 'og:image', content: ogImageUrl }),
-    React.createElement('meta', { key: 'og-image-type', property: 'og:image:type', content: 'image/png' }),
-    // Twitter
-    React.createElement('meta', { key: 'twitter-card', name: 'twitter:card', content: 'summary_large_image' }),
-    React.createElement('meta', { key: 'twitter-image', name: 'twitter:image', content: ogImageUrl }),
-    // Generic
-    React.createElement('meta', { key: 'image', name: 'image', content: ogImageUrl }),
-  ])
+exports.onRenderBody = ({ setHtmlAttributes }) => {
+  // Set document language; page-level meta tags are handled in src/components/seo.js
+  setHtmlAttributes({ lang: `ko` })
 }
