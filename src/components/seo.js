a00c39ac5d56a5ba8a3619fda0350de120fd1ec8
주은희 <eunhee.ju@jh-solution.net>
2026-01-02 13:25:29 +0900

    feature: Update opengraph content

--- src/components/seo.js
+++ src/components/seo.js
@@ -7,8 +7,10 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { useLocation } from "@reach/router"
 
 function Seo({ description, title, children, image }) {
+  const { pathname } = useLocation()
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -17,29 +19,45 @@ function Seo({ description, title, children, image }) {
             title
             description
             author
+            siteUrl
           }
         }
       }
     `
   )
 
+  const siteName = site.siteMetadata?.title
+  const siteUrl = site.siteMetadata?.siteUrl?.replace(/\/$/, "") || ""
+
   const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
-  const ogImage = image || '/images/og-image.png'
+  const fullTitle = title ? `${title} | ${siteName}` : siteName
+  const ogUrl = `${siteUrl}${pathname || ""}`
+
+  // Absolute image URL (prefer explicit image, fallback to default)
+  const ogImage = image
+    ? (image.startsWith("http") ? image : `${siteUrl}${image}`)
+    : `${siteUrl}/images/og-image.png`
 
   return (
     <>
-      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+      <title>{fullTitle}</title>
+      <link rel="canonical" href={ogUrl} />
+
+      {/* Open Graph */}
       <meta name="description" content={metaDescription} />
-      <meta property="og:title" content={title} />
+      <meta property="og:title" content={fullTitle} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
       <meta property="og:image" content={ogImage} />
-      <meta property="og:site_name" content={title} />
+      <meta property="og:url" content={ogUrl} />
+      <meta property="og:site_name" content={siteName} />
+      <meta property="og:locale" content="ko_KR" />
+
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content={ogImage} />
       <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
-      <meta name="twitter:title" content={title} />
+      <meta name="twitter:title" content={fullTitle} />
       <meta name="twitter:description" content={metaDescription} />
       {children}
     </>
