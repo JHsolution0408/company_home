0fe20c4e918070643bf330e8395cc435c828be28
jh-jeehoon-park <jeehoon.park@jh-solution.net>
2026-01-13 11:01:47 +0900

    feature: Update main page text punctuation and spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -350,7 +350,7 @@ const IndexPage = ({ data }) => {
           <h2 className={styles.title}>
             데이터 공학으로 완성된
             <br />
-            <span>JH솔루션의 통합플랫폼을 경험하세요</span>
+            <span>JH솔루션의 통합플랫폼을 경험하세요.</span>
           </h2>
           <p className={styles.description}>
             에너지 소비패턴을 예측하고, AI가 CFD 시뮬레이션을 학습하여 최적의
@@ -497,10 +497,10 @@ const IndexPage = ({ data }) => {
             <h2 className={styles.title}>
               산업의 변화와 흐름을 주도하는
               <br />
-              <span>JH솔루션의 새로운 소식을 만나보세요</span>
+              <span>JH솔루션의 새로운 소식을 만나보세요.</span>
             </h2>
             <p className={styles.description}>
-              언론이 주목한 혁신 기술부터 최신 프로젝트 수주까지, JH 솔루션이
+              언론이 주목한 혁신 기술부터 최신 프로젝트 수주까지, JH솔루션이
               창출하는 가치를 생생하게 전달합니다.
             </p>
           </div>
